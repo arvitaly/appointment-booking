2241eb3049e5232ffa68977bdd37d06bf26063b4
arvitaly <arvitaly@list.ru>
2021-01-16 12:31:21 +0800

    removed top menu

--- web/Header.tsx
+++ web/Header.tsx
@@ -12,16 +12,6 @@ export default function Header({
           Dr. {doctorLastName}
           <div className="header__brand_sub_title">{doctorSpecialty}</div>
         </div>
-        <nav className="header__nav">
-          <ul className="header__menu_list">
-            <li className="header__menu_list_item">
-              <a href="#booking">Booking</a>
-            </li>
-            <li className="header__menu_list_item">
-              <a href="#office">Office</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
